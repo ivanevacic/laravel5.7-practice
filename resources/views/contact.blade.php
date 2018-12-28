ae00aa6a3f5feb79ef73771cba8767541afc6dd8
ivanevacic <ivanevacicweb@gmail.com>
2018-12-28 18:31:07 +0100

    basic routing

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>CONTACT PAGE LET'S GO</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//  New Route with it's view ->resources/views/...
+Route::get('/contact', function () {
+    return view('contact');
+});
